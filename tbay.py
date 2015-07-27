e15071e644a2ccda7715ce50740ef30c6c725804
ddeguzman <ddeguzman@twilio.com>
2015-07-27 22:47:34 +0000

    adding tbay

---
+++ tbay.py
@@ -0,0 +1,36 @@
+#/usr/bin/python
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+from sqlalchemy import Column, Integer, String, DateTime
+
+engine = create_engine('postgresql://ubuntu:thinkful@localhost:5432/tbay')
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+class Item(Base):
+    __tablename__ = "items"
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    description = Column(String) 
+    start_time = Column(DateTime, default=datetime.utcnow)
+    
+class User(Base):
+    __tablename__ = "user"
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+    
+class Bid(Base):
+    __tablename__ = "bid"
+    
+    id = Column(Integer, primary_key=True)
+    FloatingPrice = Column(Integer, nullable=False)
+    
+Base.metadata.create_all(engine)    
+
