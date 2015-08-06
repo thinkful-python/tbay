a56835e3556d6bcee2704dcee1716ac0a5651ada
ddeguzman <ddeguzman@twilio.com>
2015-08-06 06:09:12 +0000

    adding users and main file

---
+++ main.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+from tbay import session, Item, User, Bid
+
+chris = User() 
+chris.username = "rosuav" 
+chris.password = "thinkful"
+session.add(chris)
+session.commit()
+
+ashley = User(username = "ashybon", password = "Ashl3y")
+session.add(ashley)
+session.commit()
+
+lianna = User(username = "nuki", password = "liannaliannal1ann4")
+session.add(lianna)
+session.commit()
+
+baseball = Item()
+baseball.name = "Giants World Series 2014"
+baseball.description = "Foul ball in game 2"
+chris.items.append(baseball)
+session.add(baseball)
+session.commit()
+
+guitar = Item(name = "Fender", description = "rock out with this electric guitar")
+ashley.items.append(guitar)
+session.add(guitar)
+session.commit()
+
+bid_ashley = Bid(price = 5000, item = baseball, bidder = ashley)
+session.add(bid_ashley)
+session.commit()
+
+bid_lianna = Bid(price = 2500, item = baseball, bidder = lianna)
+session.add(bid_lianna)
+session.commit()
--- tbay.py
+++ tbay.py
@@ -18,26 +18,26 @@ class Item(Base):
     name = Column(String, nullable=False)
     description = Column(String) 
     start_time = Column(DateTime, default=datetime.utcnow)
-    bids = relationship("bid", backref="item")
-    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    bids = relationship("Bid", backref="item")
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     
 class User(Base):
-    __tablename__ = "user"
+    __tablename__ = "users"
     
     id = Column(Integer, primary_key=True)
     username = Column(String, nullable=False)
     password = Column(String, nullable=False)
-    items = relationship("item", backref="seller")
-    bids = relationship("bid", backref="bidder")
+    items = relationship("Item", backref="seller")
+    bids = relationship("Bid", backref="bidder")
     
 class Bid(Base):
-    __tablename__ = "bid"
+    __tablename__ = "bids"
     
     id = Column(Integer, primary_key=True)
-    FloatingPrice = Column(Integer, nullable=False)
-    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
-    item_id = Column(Integer, ForeignKey('item.id'), nullable=False)
+    price = Column(Integer, nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    item_id = Column(Integer, ForeignKey('items.id'), nullable=False)
     
-#Base.metadata.drop_all(engine)
+Base.metadata.drop_all(engine)
 Base.metadata.create_all(engine)    
 
